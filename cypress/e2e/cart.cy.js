2a76010ca84b49d08c3ad153a84384cacb03eb38
MaharjanNaffi <nafisha078@academiacollege.edu.np>
2025-12-09 13:46:32 +0545

    edited testcases

--- cypress/e2e/cart.cy.js
+++ cypress/e2e/cart.cy.js
@@ -19,7 +19,7 @@ describe("Cart Tests - Dynamic Product Capture", () => {
         cart.verifyCartBadgeCount(1)
     })
 
-    it("TC-CART-003: Add two products & verify cart count", () => {
+    it.only("TC-CART-003: Verify cart badge updates after deleting produts", () => {
         cart.addFirstProduct()
         cart.addSecondProduct()
 
@@ -27,6 +27,10 @@ describe("Cart Tests - Dynamic Product Capture", () => {
         cart.clickCartIcon()
 
         cart.verifyCartItemCount(2)
+        cart.removeProductFromCart()
+
+        cart.verifyCartBadgeCount(1)
+        cart.verifyCartItemCount(1)
     })
 
     it("TC-CART-004: Verify dynamically added products inside cart", () => {
@@ -36,32 +40,37 @@ describe("Cart Tests - Dynamic Product Capture", () => {
         cart.clickCartIcon()
         cart.verifyCartContainsAddedProducts()
     })
+    it("TC-CART-005: To verify product appears in same order as user added in the cart", () => {
+        cart.checkProductOrderInCart()
+        
+    })
+    
 
-    it("TC-CART-005: To verify users can remove product from the cart", () => {
+    it("TC-CART-006: To verify users can remove product from the cart", () => {
         cart.addFirstProduct()
         cart.clickCartIcon()
         cart.removeProductFromCart()   
     })
-    it("TC-CART-006: Verify continue shopping button functionality", () => {
+    it("TC-CART-007: Verify continue shopping button functionality", () => {
         cart.addFirstProduct()
         cart.clickCartIcon()
         cart.clickcontinueShopping()
         cy.url().should('include', '/inventory.html')
     })
-    it("TC-CART-007: To verify users can view the product detail in cart", () => {
+    it("TC-CART-008: To verify users can view the product detail in cart", () => {
         cart.addFirstProduct()
         cart.addSecondProduct()
         cart.clickCartIcon()
         cart.clickProductDetail()
     })
-    it("TC-CART-008: To verify users can view Cart page after clicking back to product", () => {
+    it("TC-CART-009: To verify users can view Cart page after clicking back to product", () => {
         cart.addFirstProduct()
         cart.clickCartIcon()
         cart.clickProductDetail()
         cart.clickBackToProducts()
         cy.url().should('include', '/cart.html')
     })
-    it("TC-CART-009: To verify users can view chekout button", () => {
+    it("TC-CART-010: To verify users can view chekout button", () => {
         cart.addFirstProduct()
         cart.clickCartIcon()
         cart.assertCheckoutButtonVisible()
